cd68df599ce075d9b77a3d3c719ac905bea1c4be
Alessandro Oggioni <oggioniale@gmail.com>
2021-05-26 09:43:18 +0200

    added the sidebar_content for all box

--- uis/contributeDirectiveUI.R
+++ uis/contributeDirectiveUI.R
@@ -10,9 +10,14 @@ tabItem(
       collapsible = TRUE,
       enable_sidebar = TRUE,
       sidebar_width = 25,
-      sidebar_start_open = FALSE,
+      sidebar_start_open = TRUE,
       sidebar_content = tagList(
-        tags$p("..."),
+        tags$p(
+          'This box provides the general information as name of the site, institution managing the site and their addess.',
+          'In the case of Natura 2000 sites the information are collected directly from triplestore of European Environmental Agency (EEA, e.g. ',
+          tags$a("Cres - Lošinj", href="https://eunis.eea.europa.eu/sites/HR3000161", target="_blank"),
+          '). For eLTER sites, ate the moment, only the title is visible. In both cases other information can be reached following the link, by cliking on the site name.'
+        ),
         tags$p(tags$b("Press the gear for collaps this slidebar and start with the work."))
       ),
       column(12, htmlOutput("siteInfoContrib")
@@ -20,16 +25,20 @@ tabItem(
     ),
     boxPlus(
       width = 12,
-      title = "", 
+      title = "Contribute to the Marine Strategy Framework Directive (MSFD)", 
       closable = FALSE, 
       status = "info", 
       solidHeader = FALSE, 
       collapsible = TRUE,
       enable_sidebar = TRUE,
       sidebar_width = 25,
-      sidebar_start_open = FALSE,
+      sidebar_start_open = TRUE,
       sidebar_content = tagList(
-        tags$p("..."),
+        tags$p(
+          'This graph show the site contribute to the MSFD. The parameters listed with the green checked circle represent the parameters monitored in the site that
+          match with those deemed necessary to contribute to the MSFD criteria. The dark blue are all the MSFD criteria to which the parameters can contribute. 
+          The lines are meant to represent the connection between the parameters and the criteria.'
+        ),
         tags$p(tags$b("Press the gear for collaps this slidebar and start with the work."))
       ),
       column(12, visNetwork::visNetworkOutput("visNetworkPlot")
@@ -45,9 +54,12 @@ tabItem(
         collapsible = TRUE,
         enable_sidebar = TRUE,
         sidebar_width = 25,
-        sidebar_start_open = FALSE,
+        sidebar_start_open = TRUE,
         sidebar_content = tagList(
-          tags$p("..."),
+          tags$p(
+            'The table reproduces the graph above. The parameters monitored and the MSFD criteria. 
+            A link to the ECOSS thesaurus for parameters and MSFD criteria has provided.'
+          ),
           tags$p(tags$b("Press the gear for collaps this slidebar and start with the work."))
         ),
         column(12, DT::dataTableOutput('tblContrib')
